215cdb88f6d0ccb1a998f9386905e25f74f7d4b6
kingkazuma7 <your.nasionasio7@gmail.com>
2025-02-01 12:18:36 +0900

    1. 編集ボタンで編集モードになる まで

--- components/TodoListView/TodoListView.vue
+++ components/TodoListView/TodoListView.vue
@@ -1,10 +1,30 @@
 <script setup>
+import { ref } from 'vue';
+import { statuses } from "../../src/const/status";
 const props = defineProps({
   items: {
     type: Array,
     required: true
   }
 });
+
+/**
+ * localStorageがない時のケア
+ * items反映されない..refがないから更新されない
+ */
+const isLocalStorageAvailable = typeof localStorage !== 'undefined';
+let items = ref(JSON.parse((isLocalStorageAvailable && localStorage.getItem("items"))) || []);
+  let inputContent = ref(); // 内容
+  let inputLimit = ref(); // 期限
+  let inputState = ref(); // ステータス
+
+const onEdit = (id) => {
+  inputContent.value = items.value[id].content;
+  inputLimit.value = items.value[id].limit;
+  inputState.value = items.value[id].state;
+  items.value[id].onEdit = true;
+  // console.log(items.value[id].onEdit = true);
+}
 </script>
 
 <template>
@@ -23,10 +43,28 @@ const props = defineProps({
       <tbody>
         <tr v-for="item in items" :key="item.id">
           <td>{{ item.id }}</td>
-          <td>{{ item.content }}</td>
-          <td>{{ item.limit }}</td>
-          <td>{{ item.state.value }}</td>
-          <td><button>編集</button></td>
+          <td>
+            <!-- 初期false, 反転で内容表示、trueの時だけinput表示 -->
+            <span v-if="!item.onEdit">{{ item.content }}</span> 
+            <input v-else type="text" v-model="inputContent">
+          </td>
+          <td>
+            <span v-if="!item.onEdit">{{ item.limit }}</span>
+            <input v-else type="date" v-model="inputLimit">
+          </td>
+          <td>
+            <span v-if="!item.onEdit">{{ item.state.value }}</span>
+            <select v-model="inputState">
+              <option
+                v-for="state in statuses"
+                :key="state.id"
+                :value="state"
+              >
+                {{ state.value }}
+              </option>
+            </select>
+          </td>
+          <td><button @click="onEdit(item.id)">編集</button></td>
           <td><button>削除</button></td>
         </tr>
       </tbody>
